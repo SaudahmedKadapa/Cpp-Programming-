f8ab086d0c553650c37ddd67d9b86a8b4cbcd217
saudahmedkadapa <kadapasaud@gmail.com>
2024-04-16 21:59:01 +0530

    Cpp Programming

--- AlphabetPyramid.cpp
+++ AlphabetPyramid.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
     int rows;
-    cout << "Enter the number Rows="<<endl;
+    cout<<"Enter the number of Rows"<<endl;
     cin >> rows;
-    for(int i=0;i<rows;i++)
+    for(int i=0;i<=rows;i++)
     {
         for(int j=0;j<=i;j++)
         {
-            cout << i+1;
+            cout<<(char)('A'+i);
         }
         cout<<endl;
     }
-    return 0;
 
+    return 0;
 
 }
